d3820b1be837aa0df9dae85c37b73743774a148e
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-02-16 16:14:28 +0200

    Fix deploy script

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,8 @@
 import consola from "consola";
 import { ApplicationCommandType, ContextMenuCommandBuilder, REST, Routes, SlashCommandBuilder } from "discord.js";
+import * as dotenv from "dotenv";
 import { Client } from "./client.js";
+dotenv.config();
 export async function deploy(guildId: string) {
 	const client = new Client();
 	await client.load();
@@ -16,12 +18,12 @@ export async function deploy(guildId: string) {
 			)
 			.toJSON(),
 	);
-	const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+	const rest = new REST({ version: "10" }).setToken(process.env["TOKEN"]!);
 
-	rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), { body: [...commands, ...contexts] })
+	rest.put(Routes.applicationGuildCommands(process.env["CLIENT_ID"]!, guildId), { body: [...commands, ...contexts] })
 		.then((data) => {
 			if (Array.isArray(data)) consola.success(`Successfully registered ${data.length} application commands.`);
 		})
 		.catch((...params: unknown[]) => consola.error(params.shift(), ...params));
 }
-await deploy(process.env.GUILD_ID);
+await deploy(process.env["GUILD_ID"]!);
