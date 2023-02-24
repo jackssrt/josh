b3d521cff2b583529c6223c7e30dd600436f2136
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-02-24 07:48:18 +0200

    Actually call the installDependencies function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as dotenv from "dotenv";
 import { Client } from "./client.js";
+import installDependencies from "./dependencies.js";
 import { IS_REPLIT } from "./env.js";
-if (IS_REPLIT) import("./keepalive.js");
 dotenv.config();
+await installDependencies();
+if (IS_REPLIT) import("./keepalive.js");
 const client = new Client();
 await client.load();
 await client.start();
