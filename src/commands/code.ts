444a44a76224193e25666d2d264fd48b53b87630
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-03-14 23:43:17 +0200

    Add optional hidden argument to code command

--- src/commands/code.ts
+++ src/commands/code.ts
@@ -2,10 +2,19 @@ import { randomInt } from "crypto";
 import type Command from "../command.js";
 
 export default {
-	data: (b) => b.setDescription("Generates a random 4 digit room join code"),
+	data: (b) =>
+		b
+			.setDescription("Generates a random 4 digit room join code")
+			.addBooleanOption((b) =>
+				b.setName("hidden").setDescription("Should your code be only shown to you?").setRequired(false),
+			),
 	async execute({ interaction }) {
-		await interaction.reply(
-			`Your random join code is: \`${randomInt(0, 9)}${randomInt(0, 9)}${randomInt(0, 9)}${randomInt(0, 9)}\``,
-		);
+		await interaction.reply({
+			content: `Your random join code is: \`${randomInt(0, 9)}${randomInt(0, 9)}${randomInt(0, 9)}${randomInt(
+				0,
+				9,
+			)}\``,
+			ephemeral: interaction.options.getBoolean("hidden", false) ?? false,
+		});
 	},
 } as Command;
