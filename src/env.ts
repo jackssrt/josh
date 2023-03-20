44cd5adeca0e29ba06c8443cc0f9967458740d01
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-03-20 22:03:08 +0200

    Fix IS_PROD in env.ts

--- src/env.ts
+++ src/env.ts
@@ -18,13 +18,13 @@ export interface ProcessEnv {
 	MEMBER_ROLE_ID: string;
 	JOIN_LEAVE_CHANNEL_ID: string;
 	REPLIT_DB_URL: string | undefined;
-	REPLIT_ID: string | undefined;
+	REPL_ID: string | undefined;
 }
 
 export default function getEnv<T extends keyof ProcessEnv>(key: T): ProcessEnv[T] {
 	return process.env[key] as ProcessEnv[T];
 }
 
-export const IS_PROD = !!getEnv("REPLIT_ID");
+export const IS_PROD = !!getEnv("REPL_ID");
 export const IS_DEV = !IS_PROD;
 export const IS_BUILT = path.extname(import.meta.url) !== ".ts";
