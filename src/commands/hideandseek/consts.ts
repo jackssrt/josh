17676fd18ffaf8a6b85ca7771fe284d657a07191
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-03-25 20:44:09 +0200

    Update hide and seek rules

--- src/commands/hideandseek/consts.ts
+++ src/commands/hideandseek/consts.ts
@@ -17,7 +17,7 @@ ${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}Killer wail 5.1
 ${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}Wave breaker
 ${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}Kraken
 ${BULLET_EMOJI}No ninja squid.
-${BULLET_EMOJI}No where the seekers can't reach you.
+${BULLET_EMOJI}No hiding where the seekers can't reach you.
 
 ${BULLET_EMOJI}First the hiders will pick their hiding spots
 ${BULLET_EMOJI}After 1 minute in turf war or 2 minutes in ranked,
@@ -26,13 +26,14 @@ ${EMPTY_EMOJI}the seekers will go look for the hiders.
 ${BULLET_EMOJI}**Seekers ${ROLE_ICON_MAP[PlayerRole.Seeker]}**
 ${EMPTY_EMOJI}${SUB_EMOJI}${BULLET_EMOJI}aren't allowed to use the map during hiding time
 ${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}aren't allowed to use sub weapons while seeking for hiders
-${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}are allowed to super jump to squid beacons and big bubblers
+${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}are only allowed to super jump to squid beacons or big bubblers
 ${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}win if they splat all hiders once
 
 ${BULLET_EMOJI}**Hiders ${ROLE_ICON_MAP[PlayerRole.Hider]}**
 ${EMPTY_EMOJI}${SUB_EMOJI}${BULLET_EMOJI}aren't allowed to use the map
-${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}are allowed to fight back with their main weapons, sub weapons, special weapons if they get found
-${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}win if they survive until the match ends`;
+${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}are allowed to fight back with their main weapons, sub weapons and special weapons if they get found
+${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}win if they survive until the match ends
+${EMPTY_EMOJI}${EMPTY_EMOJI}${BULLET_EMOJI}aren't allowed to distract the seekers after they get splatted`;
 export const SEEKER_EXPLANATION = `${BULLET_EMOJI}As a seeker you're first going to back up and face away from the map.
 ${BULLET_EMOJI}Meanwhile the hiders are going to be painting the map and picking their hiding spots...
 ${BULLET_EMOJI}Only after hiding time is up can you start seeking!
