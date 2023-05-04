dee2a9200434a8ea31ef815f12dc743a397198fc
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-05-04 13:50:00 +0300

    Add tts channel

--- src/env.ts
+++ src/env.ts
@@ -21,6 +21,7 @@ export interface ProcessEnv {
 	REPL_ID: string | undefined;
 	CREATE_MATCH_CHANNEL_ID: string;
 	MATCH_CHANNEL_CATEGORY_ID: string;
+	TTS_CHANNEL_ID: string;
 }
 
 export default function getEnv<T extends keyof ProcessEnv>(key: T): ProcessEnv[T] {
