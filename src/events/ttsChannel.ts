a54662d00898a535a98d7b8f8bbffbca27650db6
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-05-04 14:17:03 +0300

    Make bot disconnect from empty voice channels

--- src/events/ttsChannel.ts
+++ src/events/ttsChannel.ts
@@ -17,52 +17,62 @@ const lock = new Lock();
 const SPEAK_REGEX =
 	/((https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*))|^\[[a-z]+\]|<:|:\d+>|<id:\w+>|[^A-Za-z\d\s.,!&?;:'"\-()@À-ÿ]/g;
 
-export default {
-	event: "messageCreate",
-	async on(_, message) {
-		if (
-			message.channelId !== getEnv("TTS_CHANNEL_ID") ||
-			message.author.bot ||
-			!message.inGuild() ||
-			!message.member?.voice.channel
-		)
-			return;
-		const memberName = message.member.displayName.replace(SPEAK_REGEX, "");
-		const memberVoiceChannel = message.member.voice.channel;
-		const content = message.cleanContent.replace(SPEAK_REGEX, "");
-		if (content === "") return;
-		const text = `${lastName !== memberName ? `${memberName} says ` : ""}${content}`;
-		const lockKey = await lock.lock();
-		const [player, resource] = await parallel(
-			async () => {
-				const connection =
-					(memberVoiceChannel.id === (await message.guild.members.fetchMe()).voice.channelId &&
-						getVoiceConnection(message.guildId)) ||
-					joinVoiceChannel({
-						channelId: memberVoiceChannel.id,
-						guildId: message.guildId,
-						adapterCreator: message.channel.guild.voiceAdapterCreator,
-					});
-				const player = createAudioPlayer();
-				connection.subscribe(player);
-				return player;
-			},
-			async () => {
-				const tts = Buffer.concat(
-					(
-						await getAllAudioBase64(text, {
-							lang: "en",
-						})
-					).map((result) => Buffer.from(result.base64, "base64")),
-				);
-				lastName = memberName;
-				const resource = createAudioResource(Readable.from(tts), { inlineVolume: true });
-				resource.volume?.setVolume(0.2);
-				return resource;
-			},
-		);
-		player.play(resource);
-		await awaitEvent(player, AudioPlayerStatus.Idle, 30);
-		lock.unlock(lockKey);
-	},
-} as Event<"messageCreate">;
+export default [
+	{
+		event: "messageCreate",
+		async on(_, message) {
+			if (
+				message.channelId !== getEnv("TTS_CHANNEL_ID") ||
+				message.author.bot ||
+				!message.inGuild() ||
+				!message.member?.voice.channel
+			)
+				return;
+			const memberName = message.member.displayName.replace(SPEAK_REGEX, "");
+			const memberVoiceChannel = message.member.voice.channel;
+			const content = message.cleanContent.replace(SPEAK_REGEX, "");
+			if (content === "") return;
+			const text = `${lastName !== memberName ? `${memberName} says ` : ""}${content}`;
+			const lockKey = await lock.lock();
+			const [player, resource] = await parallel(
+				async () => {
+					const connection =
+						(memberVoiceChannel.id === (await message.guild.members.fetchMe()).voice.channelId &&
+							getVoiceConnection(message.guildId)) ||
+						joinVoiceChannel({
+							channelId: memberVoiceChannel.id,
+							guildId: message.guildId,
+							adapterCreator: message.channel.guild.voiceAdapterCreator,
+						});
+					const player = createAudioPlayer();
+					connection.subscribe(player);
+					return player;
+				},
+				async () => {
+					const tts = Buffer.concat(
+						(
+							await getAllAudioBase64(text, {
+								lang: "en",
+							})
+						).map((result) => Buffer.from(result.base64, "base64")),
+					);
+					lastName = memberName;
+					const resource = createAudioResource(Readable.from(tts), { inlineVolume: true });
+					resource.volume?.setVolume(0.2);
+					return resource;
+				},
+			);
+			player.play(resource);
+			await awaitEvent(player, AudioPlayerStatus.Idle, 30);
+			lock.unlock(lockKey);
+		},
+	} as Event<"messageCreate">,
+	{
+		event: "voiceStateUpdate",
+		async on(_, oldState) {
+			const { voice } = await oldState.guild.members.fetchMe();
+			if (oldState.channel?.id === voice.channel?.id && voice.channel?.members.size === 1)
+				await voice.disconnect();
+		},
+	} as Event<"voiceStateUpdate">,
+];
