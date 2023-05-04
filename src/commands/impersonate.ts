d4c75b9d617f3bd2e095cb2f846ee03a30c7eb36
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-05-04 16:48:25 +0300

    Add impersonate command

---
+++ src/commands/impersonate.ts
@@ -0,0 +1,25 @@
+import { ChannelType } from "discord.js";
+import type Command from "../command.js";
+import { impersonate } from "../utils.js";
+
+export default {
+	data: (b) =>
+		b
+			.setDescription("Sends a message as someone else.")
+			.addUserOption((b) => b.setName("user").setDescription("The user to impersonate.").setRequired(true))
+			.addStringOption((b) =>
+				b.setName("content").setDescription("The content of the message.").setRequired(true),
+			),
+	ownerOnly: true,
+	async execute({ interaction, client }) {
+		if (!interaction.channel || interaction.channel.type !== ChannelType.GuildText)
+			return await interaction.reply({ content: "This command can't be run here.", ephemeral: false });
+		await impersonate(
+			client,
+			interaction.options.getUser("user", true),
+			interaction.channel,
+			interaction.options.getString("content", true),
+		);
+		await interaction.reply({ content: "✅", ephemeral: true });
+	},
+} as Command;
