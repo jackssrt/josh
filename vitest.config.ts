050756975233c84c1ffc600cb9de4ac566196d2c
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-06-08 01:33:04 +0300

    chore(vitest): switch from c8 to v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
-		coverage: { enabled: true, reporter: ["html", "text-summary"] },
+		coverage: { enabled: true, reporter: ["html", "text-summary"], provider: "v8" },
 	},
 });
