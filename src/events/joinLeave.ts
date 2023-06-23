f05c17c5593838398ad88d4b8d974f02cf9f56e6
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-06-23 09:17:20 +0300

    fix: send welcome message after discord's one

--- src/events/joinLeave.ts
+++ src/events/joinLeave.ts
@@ -1,42 +1,19 @@
 import type { Collection, GuildMember, PartialGuildMember } from "discord.js";
-import { Colors, channelMention, roleMention, userMention } from "discord.js";
+import { userMention } from "discord.js";
 import type Client from "../client.js";
-import { BOOYAH_EMOJI } from "../emojis.js";
-import getEnv from "../env.js";
 import type Event from "../event.js";
-import { dedent, embeds, formatNumberIntoNth, impersonate, membersWithRoles, parallel } from "../utils.js";
+import { impersonate, membersWithRoles } from "../utils.js";
 
 export async function onMemberJoin(client: Client<true>, member: GuildMember) {
 	const allMembers = membersWithRoles([client.memberRole]);
 	// adds the new member to the collection if they aren't already in it
 	allMembers.set(member.id, member);
-	await parallel(
-		async () => {
-			await client.generalChannel.send({
-				content: `${userMention(member.id)} ${roleMention(getEnv("GREETER_ROLE_ID"))}`,
-				...(await embeds((b) =>
-					b
-						.setTitle(`Welcome ${member.displayName}! 👋`)
-						.setDescription(
-							dedent`We're a tight knit splatoon community, we've invited (almost) everyone from splatoon!
-						Please read the rules in ${channelMention(getEnv("RULES_CHANNEL_ID"))},
-						and if you want, pick up some more roles in <id:customize>.
-						But most importantly, have fun! ${BOOYAH_EMOJI}`,
-						)
-						.setFooter({ text: `You're our ${formatNumberIntoNth(allMembers.size)} member!` })
-						.setTimestamp(new Date())
-						.setColor(Colors.Blurple),
-				)),
-			});
-		},
-		async () => {
-			await impersonate(
-				client,
-				member,
-				client.joinLeaveChannel,
-				`🟢 ${userMention(member.user.id)} joined, member #${allMembers.size}`,
-			);
-		},
+
+	await impersonate(
+		client,
+		member,
+		client.joinLeaveChannel,
+		`🟢 ${userMention(member.user.id)} joined, member #${allMembers.size}`,
 	);
 }
 
---
+++ src/events/welcomeMessage.ts
@@ -0,0 +1,37 @@
+import type { GuildMember, Message } from "discord.js";
+import { Colors, MessageType, channelMention, roleMention, userMention } from "discord.js";
+import type Client from "../client.js";
+import { BOOYAH_EMOJI } from "../emojis.js";
+import getEnv from "../env.js";
+import { dedent, embeds, formatNumberIntoNth, membersWithRoles } from "../utils.js";
+import type Event from "./../event.js";
+
+async function sendWelcomeMessage(client: Client<true>, member: GuildMember, message: Message<true>) {
+	const allMembers = membersWithRoles([client.memberRole]);
+	// adds the new member to the collection if they aren't already in it
+	allMembers.set(member.id, member);
+	await message.reply({
+		content: `${userMention(member.id)} ${roleMention(getEnv("GREETER_ROLE_ID"))}`,
+		...(await embeds((b) =>
+			b
+				.setTitle(`Welcome ${member.displayName}! 👋`)
+				.setDescription(
+					dedent`We're a tight knit splatoon community, we've invited (almost) everyone from splatoon!
+						Please read the rules in ${channelMention(getEnv("RULES_CHANNEL_ID"))},
+						and if you want, pick up some more roles in <id:customize>.
+						But most importantly, have fun! ${BOOYAH_EMOJI}`,
+				)
+				.setFooter({ text: `You're our ${formatNumberIntoNth(allMembers.size)} member!` })
+				.setTimestamp(new Date())
+				.setColor(Colors.Blurple),
+		)),
+	});
+}
+
+export default {
+	event: "messageCreate",
+	async on({ client }, message) {
+		if (!message.inGuild() || !message.member || message.type !== MessageType.UserJoin) return;
+		await sendWelcomeMessage(client, message.member, message);
+	},
+} as Event<"messageCreate">;
