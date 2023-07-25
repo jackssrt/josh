dbc18c05bff9582ba4e71d51e91908ce7ffb9d6c
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-07-25 20:04:47 +0300

    build: add pnpm to path in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+export PATH=$PATH:$HOME/.local/share/pnpm
 git pull
 echo Pulled!
 if [[ $1 -eq "--install" ]]; then
-	$HOME/.local/share/pnpm/pnpm install
+	pnpm install
 	echo Installed!
 fi
-$HOME/.local/share/pnpm/pnpm run build
+pnpm run build
 echo Built!
 sudo systemctl restart josh.service
 echo Restarted and done!
