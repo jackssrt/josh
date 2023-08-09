ef689800369a80dbb29bdcfa6d13791860adb002
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-08-09 16:35:36 +0300

    build: update update.sh script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,27 @@
 export PATH=$PATH:$HOME/.local/share/pnpm
 git pull
 echo Pulled!
-if [[ $1 -eq "--install" ]]; then
-	pnpm install
-	echo Installed!
-fi
+# Parse command-line options using getopt
+ARGS=$(getopt -o "" --long install -n "$0" -- "$@")
+eval set -- "$ARGS"
+
+while true; do
+    case "$1" in
+        --install)
+            pnpm install
+            echo "Installed!"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Invalid option"
+            exit 1
+            ;;
+    esac
+done
 pnpm run build
 echo Built!
 sudo systemctl restart josh.service
