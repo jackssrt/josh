30639a26f5d35b6a8d117c95b7b3df254672a90e
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-08-21 13:42:25 +0300

    fix(impersonate): make command deferred

--- src/commands/impersonate.ts
+++ src/commands/impersonate.ts
@@ -11,12 +11,13 @@ export default {
 				b.setName("content").setDescription("The content of the message.").setRequired(true),
 			),
 	ownerOnly: true,
+	defer: "ephemeral",
 	async execute({ interaction, client }) {
 		if (!interaction.channel || interaction.channel.type !== ChannelType.GuildText)
-			return await interaction.reply({ content: "This command can't be run here.", ephemeral: false });
+			return await interaction.editReply("This command can't be run here.");
 		const user = interaction.options.getUser("user", true);
 		const member = interaction.guild?.members.resolve(user) ?? user;
 		await impersonate(client, member, interaction.channel, interaction.options.getString("content", true));
-		await interaction.reply({ content: "✅", ephemeral: true });
+		await interaction.deleteReply();
 	},
 } as Command;
