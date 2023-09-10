49a4f0fa3950fd2b5f9cc3329d38851727ab5c49
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-09-10 14:00:33 +0300

    fix(env): change production detection

--- src/env.ts
+++ src/env.ts
@@ -43,6 +43,6 @@ declare global {
 }
 processEnvSchema.parse(process.env);
 
-export const IS_PROD = !!process.env.REPL_ID;
+export const IS_PROD = process.env.NODE_ENV === "production";
 export const IS_DEV = !IS_PROD;
 export const IS_BUILT = path.extname(import.meta.url) !== ".ts";
