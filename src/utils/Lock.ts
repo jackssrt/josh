b1f2203c83ee4304769bedc165de1a1d62301b9d
jackssrt <33695699+jackssrt@users.noreply.github.com>
2024-01-03 22:51:47 +0200

    fix(Lock): prohibit eager promises in lock()

--- src/utils/Lock.ts
+++ src/utils/Lock.ts
@@ -1,11 +1,8 @@
 export default class Lock {
 	private promise: Promise<void> | undefined = undefined;
-	public async lock(lockingPromise: Promise<void> | (() => Promise<void>)) {
-		// prettier-ignore
-		if (typeof lockingPromise === "function")
-			lockingPromise = lockingPromise();
+	public async lock(lockingPromise: () => Promise<void>) {
 		while (this.promise) await this.promise;
-		this.promise = lockingPromise.finally(() => {
+		this.promise = lockingPromise().finally(() => {
 			this.promise = undefined;
 		});
 	}
