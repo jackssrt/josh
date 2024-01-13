a720bd19e7681a795c8b1e0fc9120ad236224923
jackssrt <33695699+jackssrt@users.noreply.github.com>
2024-01-13 13:34:41 +0200

    fix(SyncSignal): fix fire() being null

--- src/utils/SyncSignal.ts
+++ src/utils/SyncSignal.ts
@@ -2,9 +2,9 @@
  * A signal to sync promises
  */
 export default class SyncSignal {
+	// fire is assigned to in the callback of new Promise below
+	public fire!: () => void;
 	public readonly promise = new Promise<never>((resolve) => (this.fire = resolve as () => void));
-	// fire is assigned to in the callback of new Promise above
-	public fire: () => void = null!;
 	/**
 	 * This exists to make SyncSignal awaitable
 	 */
