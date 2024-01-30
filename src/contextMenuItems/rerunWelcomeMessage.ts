8aba89fa6eaf83d0a0f2cdad371caf8577e37efe
jackssrt <33695699+jackssrt@users.noreply.github.com>
2024-01-30 19:10:21 +0200

    fix(rerunWelcomeMessage): remove client parameter

--- src/contextMenuItems/rerunWelcomeMessage.ts
+++ src/contextMenuItems/rerunWelcomeMessage.ts
@@ -6,13 +6,13 @@ export default createContextMenuItem({
 	type: "Message",
 	data: (b) => b,
 	ownerOnly: true,
-	execute: async ({ client, interaction }) => {
+	execute: async ({ interaction }) => {
 		if (!interaction.inCachedGuild() || interaction.targetMessage.type !== MessageType.UserJoin)
 			return await interaction.reply({
 				content: "Run this context menu item on a join message!",
 				ephemeral: true,
 			});
-		await sendWelcomeMessage(client, interaction.targetMessage);
+		await sendWelcomeMessage(interaction.targetMessage);
 		await interaction.reply({ content: "done", ephemeral: true });
 	},
 });
