1432a381f83ed34d701b255c539016d5c614113b
jackssrt <33695699+jackssrt@users.noreply.github.com>
2024-01-31 18:28:35 +0200

    style(emojis): add categories

--- src/emojis.ts
+++ src/emojis.ts
@@ -1,26 +1,38 @@
 export const BOOYAH_EMOJI = "<:booyah:1072277912100806698>";
 export const SQUID_SHUFFLE_EMOJI = "<a:squidShuffle:1072590626798960680>";
+
+// # Text icons
 export const BULLET_EMOJI = "<:bu:1202269770813014136>";
 export const EMPTY_EMOJI = "<:em:1202270329289064488>";
 export const SUB_EMOJI = "<:su:1202269818934538310>";
+export const DIVIDER_EMOJI = "<:di:1202269703335321600>";
+
+// # Hide and seek
 export const VEEMO_PEEK_EMOJI = "<:veemoPeek:1075825973020340245>";
 export const SEEKER_EMOJI = "<:seeker:1202263739076976770>";
+
+// # Battle modes
 export const CHALLENGES_EMOJI = "<:challenges:1114260747363553402>";
 export const REGULAR_BATTLE_EMOJI = "<:regularBattle:1085293922181455942>";
 export const ANARCHY_BATTLE_EMOJI = "<:anarchyBattle:1152240507603390506>";
 export const X_BATTLE_EMOJI = "<:xBattle:1152240524523216988>";
 export const SPLATFEST_EMOJI = "<:splatfest:1086786495286890506>";
-export const DIVIDER_EMOJI = "<:di:1202269703335321600>";
-export const COHOZUNA_EMOJI = "<:cohozuna:1152240510585552916>";
-export const HORRORBOROS_EMOJI = "<:horrorboros:1152240512493957271>";
-export const MEGALODONTIA_EMOJI = "<:megalodontia:1180521720315121846>";
+
+// ## Ranked modes
 export const SPLAT_ZONES_EMOJI = "<:splatZones:1071477929969721474>";
 export const CLAM_BLITZ_EMOJI = "<:clamBlitz:1071477924764598313>";
 export const RAINMAKER_EMOJI = "<:rainmaker:1071477926974992384>";
 export const TOWER_CONTROL_EMOJI = "<:towerControl:1071477928304578560>";
+
+// # Salmon run
 export const SPAWNING_GROUNDS_EMOJI = "<:spawningGrounds:1152240521910157343>";
 export const GONE_FISSION_HYDROPLANT_EMOJI = "<:goneFissionHydroplant:1152240516323344476>";
 export const MAROONERS_BAY_EMOJI = "<:maroonersBay:1152240518672158762>";
 export const SOCKEYE_STATION_EMOJI = "<:sockeyeStation:1152240519926251621>";
 export const JAMMIN_SALMON_JUNCTION_EMOJI = "<:jamminSalmonJunction:1160235437420449832>";
 export const SALMONID_SMOKEYARD_EMOJI = "<:salmonidSmokeyard:1160235434568323204>";
+
+// ## King salmonids
+export const COHOZUNA_EMOJI = "<:cohozuna:1152240510585552916>";
+export const HORRORBOROS_EMOJI = "<:horrorboros:1152240512493957271>";
+export const MEGALODONTIA_EMOJI = "<:megalodontia:1180521720315121846>";
